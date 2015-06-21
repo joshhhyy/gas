a94298b5fa9bfb1a58e91f9f9b392a8e0f5dfea4
Alan Wright <alanwright@Alans-MacBook-Pro.local>
2015-06-21 14:04:01 +1000

    fixing up routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,22 @@
 #
 # Table name: users
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  email       :string
-#  avatar      :string
-#  last_active :datetime
-#  alumni      :boolean
-#  admin       :boolean
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  avatar          :string
+#  last_active     :datetime
+#  alumni          :boolean
+#  admin           :boolean
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
 #
 
 class User < ActiveRecord::Base
   belongs_to :group
   has_many :posts
+  has_secure_password
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
 end
