d882556313e3993505baa65b5b4d84089470df29
Josh Bonador <Josh@Joshs-MacBook-Air.local>
2015-06-22 12:41:09 +1000

    More fixes on the post index and show. Fixed post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
     @post.user_id = @current_user.id
     respond_to do |format|
       if @post.save
-        format.html { redirect_to [@group, @user, @post], notice: 'Post was successfully created.' }
+        format.html { redirect_to group_user_posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
