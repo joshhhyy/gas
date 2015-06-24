d94571044ef75ec45e4addf0b0a8d6eb55f2a8a1
Alan Wright <alanwright@Alans-MacBook-Pro.local>
2015-06-24 16:19:55 +1000

    trying to fix merge conflict

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,6 @@ class SessionController < ApplicationController
   end
 
   def create 
-    
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
