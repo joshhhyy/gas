5bcfcf6cc08e93cc63a3251602dc2200c9fefceb
Alan Wright <alanwright@Alans-MacBook-Pro.local>
2015-06-25 17:06:09 +1000

    added instructor quotes to display random quotes

---
+++ app/views/quotes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @quote, :id, :quote, :group_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625030510) do
+ActiveRecord::Schema.define(version: 20150625042301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150625030510) do
     t.datetime "updated_at", null: false
     t.string   "avatar"
     t.string   "instructor"
+    t.text     "quotes"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -42,6 +43,13 @@ ActiveRecord::Schema.define(version: 20150625030510) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "cl_id"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.text     "quote"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
